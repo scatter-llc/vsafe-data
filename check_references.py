3eea58089a8a65f98472fe4647bff95b20cbb398
James Hare <jamesmhare@gmail.com>
2023-04-28 14:34:41 -0700

    write check-refs run to database

--- check_references.py
+++ check_references.py
@@ -1,41 +1,57 @@
 import requests
-import csv
 import datetime
+import pymysql
 from urllib.parse import quote
+from credentials import username, password
 
-def process_wikipedia_urls(article_urls):
+def process_wikipedia_urls(article_urls, connection):
     base_url = (
         "https://check-references.toolforge.org/v2/statistics/all?"
         "url={url}&regex=test"
     )
 
-    current_date = datetime.date.today().strftime('%Y-%m-%d')
-    output_filename = f"check-references-{current_date}.csv"
+    for article_url in article_urls:
+        encoded_article_url = quote(article_url, safe='/:')
+        request_url = base_url.format(url=encoded_article_url)
+        response = requests.get(request_url)
 
-    with open(output_filename, 'w', newline='') as csvfile:
-        csv_writer = csv.writer(
-            csvfile, delimiter=',', quotechar='"'
-        )
-        csv_writer.writerow(['Article URL', 'First Level Domain', 'URL'])
+        if response.status_code == 200:
+            data = response.json()
+            url_details = data.get("url_details", [])
 
-        for article_url in article_urls:
-            encoded_article_url = quote(article_url, safe='/:')
-            request_url = base_url.format(url=encoded_article_url)
-            response = requests.get(request_url)
+            for entry in url_details:
+                first_level_domain = entry.get("first_level_domain")
+                url = entry.get("url")
 
-            if response.status_code == 200:
-                data = response.json()
-                url_details = data.get("url_details", [])
+                with connection.cursor() as cursor:
+                    # Check if the First Level Domain exists in the domains table
+                    cursor.execute(
+                        "SELECT id FROM domains WHERE domain = %s",
+                        (first_level_domain,)
+                    )
+                    domain_id = cursor.fetchone()
 
-                for entry in url_details:
-                    first_level_domain = entry.get("first_level_domain")
-                    url = entry.get("url")
-                    csv_writer.writerow([article_url, first_level_domain, url])
-            else:
-                print(
-                    f"Error: Unable to process {article_url}, "
-                    f"status code: {response.status_code}"
-                )
+                    if domain_id is None:
+                        # If not found, insert the First Level Domain into the domains table
+                        cursor.execute(
+                            "INSERT INTO domains (domain) VALUES (%s)",
+                            (first_level_domain,)
+                        )
+                        connection.commit()
+                        domain_id = cursor.lastrowid
+
+                    # Insert a row into the urls table
+                    cursor.execute(
+                        "INSERT INTO urls (url, url_appeared_on, domain_id) VALUES (%s, %s, %s)",
+                        (url, article_url, domain_id)
+                    )
+                    connection.commit()
+
+        else:
+            print(
+                f"Error: Unable to process {article_url}, "
+                f"status code: {response.status_code}"
+            )
 
 if __name__ == "__main__":
     with open('vsafe-uniq.txt') as f:
@@ -43,4 +59,16 @@ if __name__ == "__main__":
             f"https://en.wikipedia.org/wiki/{line.strip().replace(' ', '_')}"
             for line in f
         ]
-        process_wikipedia_urls(article_urls)
+
+    connection = pymysql.connect(
+        host='tools.db.svc.wikimedia.cloud',
+        user=username,
+        password=password,
+        db='s55412__cited_urls_p',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+    try:
+        process_wikipedia_urls(article_urls, connection)
+    finally:
+        connection.close()
