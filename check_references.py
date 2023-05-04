ff965a9894db02e67f506f8bbc7ba91d0b2c967c
James Hare <jamesmhare@gmail.com>
2023-05-04 16:25:15 -0700

    new timestamp column

--- check_references.py
+++ check_references.py
@@ -1,10 +1,23 @@
 import requests
 import datetime
 import pymysql
+from tld import get_fld
 from urllib.parse import quote
 from credentials import username, password
 
+def remove_archive_prefix(url, first_level_domain):
+    pattern = r'^https://web\.archive\.org/web/(\d{14})/'
+    match = re.match(pattern, url)
+
+    if match:
+        url = re.sub(pattern, '', url)
+        first_level_domain = get_fld(url)
+    else:
+        return url, first_level_domain
+
 def process_wikipedia_urls(article_urls, connection):
+    now = int(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+
     base_url = (
         "https://check-references.toolforge.org/v2/statistics/all?"
         "url={url}&regex=test"
@@ -20,8 +33,9 @@ def process_wikipedia_urls(article_urls, connection):
             url_details = data.get("url_details", [])
 
             for entry in url_details:
-                first_level_domain = entry.get("first_level_domain")
                 url = entry.get("url")
+                first_level_domain = entry.get("first_level_domain")
+                url, first_level_domain = remove_archive_prefix(url, first_level_domain)
 
                 # Skip the row if any of the columns are missing or empty
                 if not first_level_domain or not url or not article_url:
@@ -47,12 +61,25 @@ def process_wikipedia_urls(article_urls, connection):
                         connection.commit()
                         domain_id = cursor.lastrowid
 
-                    # Insert a row into the urls table
+                    # Check if a row with the specified url and url_appeared_on already exists
                     cursor.execute(
-                        "INSERT INTO urls (url, url_appeared_on, domain_id) VALUES (%s, %s, %s)",
-                        (url, article_url, domain_id)
+                        "SELECT COUNT(*) FROM urls WHERE url = %s AND url_appeared_on = %s",
+                        (url, article_url)
                     )
-                    connection.commit()
+                    count = cursor.fetchone()[0]
+
+                    # Insert a row into the urls table if it doesn't already exist
+                    if count == 0:
+                        cursor.execute(
+                            "INSERT INTO urls (url, url_appeared_on, domain_id, last_updated) VALUES (%s, %s, %s, %s)",
+                            (url, article_url, domain_id, now)
+                        )
+                    else:
+                        cursor.execute(
+                            "UPDATE urls SET last_updated = %s WHERE url = %s AND url_appeared_on = %s",
+                            (now, url, article_url)
+                        )
+
 
         else:
             print(
